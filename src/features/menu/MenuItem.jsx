34ca26105f023062b09d2b9238fe59b2d9923d1b
YAcodingroom <n12a18a@gmail.com>
2024-07-28 16:17:10 +0800

    feat: add loader to get data

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,18 +1,23 @@
-function MenuItem({ pizza }) {
-  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+import PropTypes from 'prop-types'
+import { formatCurrency } from '../../utils/helpers'
 
-  return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
-        </div>
-      </div>
-    </li>
-  );
+MenuItem.propTypes = { pizza: PropTypes.object }
+
+function MenuItem({ pizza }) {
+	const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza
+
+	return (
+		<li>
+			<img src={imageUrl} alt={name} />
+			<div>
+				<p>{name}</p>
+				<p>{ingredients.join(', ')}</p>
+				<div>
+					{!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+				</div>
+			</div>
+		</li>
+	)
 }
 
-export default MenuItem;
+export default MenuItem
