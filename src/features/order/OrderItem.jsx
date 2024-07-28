627c3609f0f26529176fb1a003ddd78974e0c19e
YAcodingroom <n12a18a@gmail.com>
2024-07-28 13:05:21 +0800

    build: add initial components & static files

---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import { formatCurrency } from '../../utils/helpers'
+
+OrderItem.propTypes = {
+	item: PropTypes.any,
+	isLoadingIngredients: PropTypes.any,
+	ingredients: PropTypes.any,
+}
+
+function OrderItem({ item, isLoadingIngredients, ingredients }) {
+	const { quantity, name, totalPrice } = item
+
+	return (
+		<li>
+			<div>
+				<p>
+					<span>{quantity}&times;</span> {name}
+				</p>
+				<p>{formatCurrency(totalPrice)}</p>
+			</div>
+		</li>
+	)
+}
+
+export default OrderItem
